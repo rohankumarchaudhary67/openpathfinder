0ed6a97db05d850bf83c444fb600c7ec2f2143de
Rohan Chaudhary <rohankumarchaudhary67@gmail.com>
2024-04-10 20:06:49 +0530

    feat: Implement Core Routing for Open Path Finder Updates

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>Open Path Finder | Discover, Connect & Code</title>
+
+      {/* Metadata for SEO. */}
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Eduno (Empower Yourself with Eduno)" />
+
+      {/* Default Favicon */}
+      <link rel="icon" href="/logos/opf-wbg.png" />
+    </>
+  );
+}
