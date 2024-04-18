e21736fb73fff25196a8f6dfb65a03f1ec713c57
Rohan Chaudhary <rohankumarchaudhary67@gmail.com>
2024-04-18 22:50:46 +0530

    add content

--- app/head.tsx
+++ app/head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
 
       {/* Metadata for SEO. */}
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Eduno (Empower Yourself with Eduno)" />
+      <meta name="description" content="Open Path Finder" />
 
       {/* Default Favicon */}
       <link rel="icon" href="/logos/opf-wbg.png" />
