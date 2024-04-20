a0caafeab8edfd2278ea111b8d6276986bf57f60
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-04-20 15:30:07 +0530

    AboutUs_page

---
+++ app/about/page.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./about.css"
+
+export default function page() {
+  return (
+    <div className=''>
+    <div className='grid place-items-center'>
+  <img src='logos/opf-wbg.png' alt='logo'/>   
+</div>
+
+    <div className='px-5 mb-7 text-sm md:px-36'>
+        <p><div className='bld'>About Us</div>
+<div className='bld'>Executive Summary:</div>
+Open Path Finder is a pioneering platform revolutionizing how beginner developers engage with open source projects. Our user-friendly interface offers tailored project recommendations, empowering aspiring developers to contribute meaningfully to the open source community. Led by industry experts, we're committed to democratizing access to open source contributions while fostering sustainable growth and innovation in the tech industry.
+
+<div className='bld'>Project Description:</div>
+Open Path Finder is dedicated to providing beginner developers with a seamless entry point into the world of open source projects. Our platform offers customized project recommendations, advanced search filters, community collaboration tools, and progress tracking features to support developers at every step of their journey.
+
+<div className='bld'>Project Features:</div>
+Customized Project Recommendations: Our platform offers personalized project recommendations based on the user's interests, skill level, and chosen technology stack.
+<div className='bld'>Advanced Search Filters:</div> Users can utilize advanced search filters to narrow down their project options based on criteria such as programming language, project size, activity level, and more.
+Community Collaboration Tools: Connect with other developers working on similar projects, share knowledge, and collaborate on open-source initiatives.
+-Progress 
+<div className='bld'>Tracking and Feedback:</div> Track your progress as you contribute to open-source projects through our platform.
+
+<div className='bld'>The Future of Open Path Finder:</div>
+The future of Open Path Finder is poised to continue breaking barriers, revolutionizing developer engagement with open source projects, and becoming the go-to platform for aspiring developers worldwide.</p>
+    </div>
+
+    </div>
+  )
+}
