de49242f972630367f7e39e98f953fd91fb2bb9b
Rohan Chaudhary <rohankumarchaudhary67@gmail.com>
2024-06-22 14:26:07 +0530

    Mentor page redesigned

--- src/components/dashboard/mentors/index.tsx
+++ src/components/dashboard/mentors/index.tsx
@@ -4,24 +4,13 @@ export default function FindMentorComp() {
     return (
         <>
             <div className="md:container py-4">
-                <div className="text-gray-600 body-font">
-                    <div className="container py-4 mx-auto">
-                        <div className="flex flex-wrap -m-2">
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
-                            <SingleMentorCard />
+                <section className="bg-transparent dark:bg-gray-900">
+                    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+                        <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+                            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Mentor</h2>
+                            <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Connect with our mentor to explore open source and get guidance for contribution.</p>
+                        </div>
+                        <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
                             <SingleMentorCard />
                             <SingleMentorCard />
                             <SingleMentorCard />
@@ -30,7 +19,7 @@ export default function FindMentorComp() {
                             <SingleMentorCard />
                         </div>
                     </div>
-                </div>
+                </section>
             </div>
         </>
     )
